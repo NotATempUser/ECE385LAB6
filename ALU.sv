106f984edeb6559c186dd444d945d7c433bb62ba
trooper306 <61365500+trooper306@users.noreply.github.com>
2020-02-28 14:22:48 -0600

    Adding Finished ALU
    SR2MUX=0 connects to regfile and SR2MUX=1 connects to IR

---
+++ ALU.sv
@@ -0,0 +1,28 @@
+module ALU(input logic [1:0] ALUK,
+			  input logic [15:0] IR, SR1,SR2,
+			  input logic SR2MUX,
+			  output logic [15:0] OUT);
+			  
+		logic [15:0] B;
+			  
+	// (00 ADD)(01 AND)(11 NOT)
+	always_comb
+	begin
+		
+	
+		case (SR2MUX)										
+			default	: B = SR2 ;
+			1'b1		: B = { {10{IR[5]}}, IR[5:0]};
+		endcase
+	
+		case (ALUK)
+			2'b00			: OUT=SR1+B;
+			2'b01			: OUT=SR1&B;
+			2'b11			: OUT=~SR1;
+			default		: OUT=SR1;
+		endcase
+
+
+	end
+
+endmodule
